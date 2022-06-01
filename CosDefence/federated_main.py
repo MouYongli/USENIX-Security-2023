c185b28e17f952a2719dad34e6c9597a469ae8de
Yongli Mou <mouyongli@hotmail.com>
2022-06-01 17:30:16 +0200

    update fed train

--- CosDefence/federated_main.py
+++ CosDefence/federated_main.py
@@ -16,20 +16,21 @@ import torchvision.transforms as transforms
 from torch.autograd import Variable
 
 from models import MLP, LeNet, AlexNet, ResNet
+from sampling import get_mnist_iid, get_mnist_noniid, get_cifar10_iid, get_cifar10_noniid
 
 if __name__ == "__main__":
     here = osp.dirname(osp.abspath(__file__))
     parser = argparse.ArgumentParser(description="CosDefence: A Defence against Data Poisoning Attacks in Federated Learning")
     parser.add_argument('--dataset', type=str, default='mnist', help='dataset benchmark to use', choices=["mnist", "cifar10"])
     parser.add_argument('--model', type=str, default='lenet', help='neural network model to use', choices=["mlp", "lenet", "alexnet", "resnet"])
-    parser.add_argument('--distribution', type=str, default='dirichlet', help='label distribution', choices=["uniform", "dirichlet", "partial"])
+    parser.add_argument('--iid', type=bool, default=False, help='iid or non-iid, default: True')
+    parser.add_argument('--distribution', type=str, default='dirichlet', help='label distribution', choices=["uniform", "equal", "dirichlet", "partial"])
     parser.add_argument('--train-batch-size', type=int, default=8, help='batch size of training')
-    parser.add_argument('--val-batch-size', type=int, default=16, help='batch size of val')
     parser.add_argument('--test-batch-size', type=int, default=16, help='batch size of test')
     parser.add_argument('--lr', type=float, default=1e-4, help='learning rate')
     parser.add_argument('--weight-decay', type=float, default=5e-4, help='weight decay')
     ## Parameters of federated learning settings
-    parser.add_argument('--num-clients', type=int, default=10, help='number of clients')
+    parser.add_argument('--num-clients', type=int, default=100, help='number of clients')
     parser.add_argument('--num-attackers', type=int, default=1, help='number of malicious attackers')
     parser.add_argument("--num-communication-rounds", type=int, default=50, help="number of federated learning communication rounds")
     parser.add_argument("--num-local-epochs", type=int, default=1, help="number of local epochs")
@@ -41,17 +42,6 @@ if __name__ == "__main__":
     cuda = torch.cuda.is_available()
     experiment_dir = osp.join(here, 'results', 'results/federated_train', args.dataset, args.model)
 
-    trans_train_mnist = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-    trans_val_mnist = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-    transform_train_cifar10 = transforms.Compose([
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])
-    transform_val_cifar10 = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])
     # load dataset and split clients
     if args.dataset == 'mnist':
         args.num_classes = 10
@@ -61,10 +51,18 @@ if __name__ == "__main__":
         args.flip_label_to = 9
         if not args.model in ["mlp", "lenet"]:
             exit('Error: only models "mlp", "lenet", support for dataset {}'.format(args.dataset))
-        dataset_train = torchvision.datasets.MNIST('../Data/mnist/', train=True, download=True, transform=trans_train_mnist)
-        dataset_test = torchvision.datasets.MNIST('../Data/mnist/', train=False, download=True, transform=trans_val_mnist)
-        dataloader_train = torch.utils.data.DataLoader(dataset_train, batch_size=args.train_batch_size, shuffle=True)
-        dataloader_test = torch.utils.data.DataLoader(dataset_test, batch_size=args.test_batch_size, shuffle=False)
+        transform_train_mnist = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+        transform_test_mnist = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+        dataset_train = torchvision.datasets.MNIST('../Data/mnist/', train=True, download=True, transform=transform_train_mnist)
+        dataset_test = torchvision.datasets.MNIST('../Data/mnist/', train=False, download=True, transform=transform_test_mnist)
+        if args.iid:
+            assert args.distribution in ["uniform", "equal"]
+            client_idx_dict, client_cls_counts = get_mnist_iid(dataset_train, distribution=args.distribution, num_clients=args.num_clients)
+        else:
+            assert args.distribution in ["dirichlet", "partial"]
+            client_idx_dict, client_cls_counts = get_mnist_noniid(dataset_train, distribution=args.distribution, num_clients=args.num_clients)
+        # dataloader_train = torch.utils.data.DataLoader(dataset_train, batch_size=args.train_batch_size, shuffle=True)
+        # dataloader_test = torch.utils.data.DataLoader(dataset_test, batch_size=args.test_batch_size, shuffle=False)
     elif args.dataset == 'cifar10':
         args.num_classes = 10
         args.classes = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'),
@@ -73,10 +71,26 @@ if __name__ == "__main__":
         args.flip_label_to = 7
         if not args.model in ["alexnet", "resnet"]:
             exit('Error: only models "alexnet", "resnet", support for dataset {}'.format(args.dataset))
+        transform_train_cifar10 = transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+        transform_val_cifar10 = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
         dataset_train = torchvision.datasets.CIFAR10('../Data/cifar', train=True, download=True, transform=transform_train_cifar10)
         dataset_test = torchvision.datasets.CIFAR10('../Data/cifar', train=False, download=True, transform=transform_val_cifar10)
-
-        dataloader_train = torch.utils.data.DataLoader(dataset_train, batch_size=args.train_batch_size, shuffle=True)
-        dataloader_test = torch.utils.data.DataLoader(dataset_test, batch_size=args.test_batch_size, shuffle=False)
+        if args.iid:
+            assert args.distribution in ["uniform", "equal"]
+            client_idx_dict, client_cls_counts = get_cifar10_iid(dataset_train, distribution=args.distribution, num_clients=args.num_clients)
+        else:
+            assert args.distribution in ["dirichlet", "partial"]
+            client_idx_dict, client_cls_counts = get_cifar10_noniid(dataset_train, distribution=args.distribution, num_clients=args.num_clients)
+        # dataloader_train = torch.utils.data.DataLoader(dataset_train, batch_size=args.train_batch_size, shuffle=True)
+        # dataloader_test = torch.utils.data.DataLoader(dataset_test, batch_size=args.test_batch_size, shuffle=False)
     else:
         exit('Error: unrecognized dataset')
+    import pprint
+    pprint.pprint(client_cls_counts)
--- CosDefence/sampling.py
+++ CosDefence/sampling.py
@@ -16,20 +16,33 @@ def record_net_data_stats(y_train, net_dataidx_map):
         net_cls_counts[net_i] = tmp
     return net_cls_counts
 
-def partition_data(dataset, num_clients=10, partition="equal", alpha=0.5):
-    num_data = len(dataset)
-    num_classes = len(np.unique(dataset.targets))
-    if partition == "homo":
+def partition_data(dataset, num_clients=10, partition="dirichlet", alpha=0.5):
+    data, targets = dataset[0], dataset[1]
+    num_data = len(data)
+    num_classes = len(np.unique(targets))
+    if partition == "uniform":
         idxs = np.random.permutation(num_data)
         batch_idxs = np.array_split(idxs, num_clients)
         client_idx_dict = {i: batch_idxs[i] for i in range(num_clients)}
-    elif partition == "dir":
+    elif partition == "equal":
+        client_idx_dict = {}
+        idx_batch = [[] for _ in range(num_clients)]
+        for k in range(num_classes):
+            idx_k = np.where(targets == k)[0]
+            np.random.shuffle(idx_k)
+            proportions = np.array([1.0 / num_clients for _ in range(num_clients)])
+            proportions = (np.cumsum(proportions) * len(idx_k)).astype(int)[:-1]
+            idx_batch = [idx_j + idx.tolist() for idx_j, idx in zip(idx_batch, np.split(idx_k, proportions))]
+        for j in range(num_clients):
+            np.random.shuffle(idx_batch[j])
+            client_idx_dict[j] = idx_batch[j]
+    elif partition == "dirichlet":
         min_size = 0
         client_idx_dict = {}
         while min_size < 10:
             idx_batch = [[] for _ in range(num_clients)]
             for k in range(num_classes):
-                idx_k = np.where(dataset.targets == k)[0]
+                idx_k = np.where(targets == k)[0]
                 np.random.shuffle(idx_k)
                 proportions = np.random.dirichlet(np.repeat(alpha, num_clients))
                 proportions = np.array([p * (len(idx_j) < num_data / num_clients) for p, idx_j in zip(proportions, idx_batch)])
@@ -40,239 +53,82 @@ def partition_data(dataset, num_clients=10, partition="equal", alpha=0.5):
         for j in range(num_clients):
             np.random.shuffle(idx_batch[j])
             client_idx_dict[j] = idx_batch[j]
-    client_cls_counts = record_net_data_stats(dataset.targets, client_idx_dict)
+    elif partition == "partial":
+        raise NotImplemented
+    else:
+        exit('Error: unrecognized distribution')
+
+    return client_idx_dict
+
+
+def get_mnist_iid(dataset, distribution="uniform", num_clients=10):
+    """
+    @param mode: iid mode - equal or homo
+    """
+    assert distribution in ["equal", "uniform"]
+    data, targets = dataset.data, dataset.targets
+    client_idx_dict = partition_data((data, targets), num_clients=num_clients, partition=distribution)
+    client_cls_counts = record_net_data_stats(targets, client_idx_dict)
     return client_idx_dict, client_cls_counts
 
 
-def get_mnist_iid(dataset, mode="homo", num_clients=10):
-    """
-    @param mode: iid mode - equal or homo
-    """
-    assert mode in ["equal", "homo"]
-    return partition_data(dataset, num_clients=num_clients, partition=mode)
-
-
-def get_mnist_noniid(dataset, mode="dir", num_clients=10):
+def get_mnist_noniid(dataset, distribution="dirichlet", num_clients=10):
     """
     @param mode: iid mode - dir or cls
     """
-    assert mode in ["dir", "cls"]
-    return partition_data(dataset, num_clients=num_clients, partition=mode, alpha=0.5)
+    assert distribution in ["dirichlet", "partial"]
+    data, targets = dataset.data, dataset.targets
+    client_idx_dict = partition_data((data, targets), num_clients=num_clients, partition=distribution, alpha=0.5)
+    client_cls_counts = record_net_data_stats(targets, client_idx_dict)
+    return client_idx_dict, client_cls_counts
 
 
-def get_cifar10_iid(dataset, mode="homo", num_clients=100):
+def get_cifar10_iid(dataset, distribution="uniform", num_clients=100):
     """
-    @param mode: iid mode - equal or homo
+    @param mode: iid mode - equal or uniform
     """
-    assert mode in ["equal", "homo"]
-    return partition_data(dataset, num_clients=num_clients, partition=mode)
+    assert distribution in ["equal", "uniform"]
+    data, targets = dataset.data, np.array(dataset.targets)
+    client_idx_dict = partition_data((data, targets), num_clients=num_clients, partition=distribution)
+    client_cls_counts = record_net_data_stats(targets, client_idx_dict)
+    return client_idx_dict, client_cls_counts
 
-
-def get_cifar10_noniid(dataset, mode="dir", num_clients=100, alpha=0.5):
+def get_cifar10_noniid(dataset, distribution="dirichlet", num_clients=100, alpha=0.5):
     """
     @param mode: iid mode - dir or cls
     """
-    assert mode in ["dir", "cls"]
-    return partition_data(dataset, num_clients=num_clients, partition=mode, alpha=0.5)
+    assert distribution in ["dirichlet", "partial"]
+    data, targets = dataset.data, np.array(dataset.targets)
+    client_idx_dict = partition_data((data, targets), num_clients=num_clients, partition=distribution, alpha=0.5)
+    client_cls_counts = record_net_data_stats(targets, client_idx_dict)
+    return client_idx_dict, client_cls_counts
 
 if __name__ == '__main__':
-    transform_train_mnist = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-    transform_test_mnist = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-    mnist_train_ds = torchvision.datasets.MNIST('../Data/mnist/', train=True, download=True, transform=transform_train_mnist)
-    mnist_test_ds = torchvision.datasets.MNIST('../Data/mnist/', train=False, download=True, transform=transform_test_mnist)
-    client_idx_dict, client_cls_counts = get_mnist_noniid(mnist_train_ds, mode="dir", num_clients=10)
+    # transform_train_mnist = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+    # transform_test_mnist = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+    # mnist_train_ds = torchvision.datasets.MNIST('../Data/mnist/', train=True, download=True, transform=transform_train_mnist)
+    # mnist_test_ds = torchvision.datasets.MNIST('../Data/mnist/', train=False, download=True, transform=transform_test_mnist)
+    # # mnist_client_idx_dict, mnist_client_cls_counts = get_mnist_noniid(mnist_train_ds, mode="dir", num_clients=10)
+    # mnist_client_idx_dict, mnist_client_cls_counts = get_mnist_iid(mnist_train_ds, mode="homo", num_clients=10)
+    # import pprint
+    # pprint.pprint(mnist_client_cls_counts)
+    # counts = 0
+    # for i in mnist_client_cls_counts.keys():
+    #     for j in mnist_client_cls_counts[i].keys():
+    #         counts = counts + mnist_client_cls_counts[i][j]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # batch_size = 8
-# train_size = 0.75
-# least_samples = batch_size / (1 - train_size)
-# # https://github.com/TsingZ0/PFL-Non-IID/blob/5c8406674d3ef4ea21fd8aca63ea530457f7bcc3/dataset/utils/dataset_utils.py
-# def separate_data(data, num_clients, num_classes, niid=True, real=True, partition=None, balance=False,
-#                   class_per_client=2):
-#     X = [[] for _ in range(num_clients)]
-#     y = [[] for _ in range(num_clients)]
-#     statistic = [[] for _ in range(num_clients)]
-#     dataset_content, dataset_label = data
-#     if partition is None or partition == "noise":
-#         dataset = []
-#         for i in range(num_classes):
-#             idx = dataset_label == i
-#             print(idx)
-#             dataset.append(dataset_content[idx])
-#         if not niid or real:
-#             class_per_client = num_classes
-#         class_num_client = [class_per_client for _ in range(num_clients)]
-#         for i in range(num_classes):
-#             selected_clients = []
-#             for client in range(num_clients):
-#                 if class_num_client[client] > 0:
-#                     selected_clients.append(client)
-#             if niid and not real:
-#                 selected_clients = selected_clients[:int(num_clients / num_classes * class_per_client)]
-#             num_all = len(dataset[i])
-#             num_clients_ = len(selected_clients)
-#             if niid and real:
-#                 num_clients_ = np.random.randint(1, len(selected_clients))
-#             num_per = num_all / num_clients_
-#             if balance:
-#                 num_samples = [int(num_per) for _ in range(num_clients_ - 1)]
-#             else:
-#                 num_samples = np.random.randint(max(num_per / 10, least_samples / num_classes), num_per, num_clients_ - 1).tolist()
-#             num_samples.append(num_all - sum(num_samples))
-#             if niid:
-#                 # each client is not sure to have all the labels
-#                 selected_clients = list(np.random.choice(selected_clients, num_clients_, replace=False))
-#             idx = 0
-#             for client, num_sample in zip(selected_clients, num_samples):
-#                 if len(X[client]) == 0:
-#                     X[client] = dataset[i][idx:idx + num_sample]
-#                     y[client] = i * np.ones(num_sample)
-#                 else:
-#                     X[client] = np.append(X[client], dataset[i][idx:idx + num_sample], axis=0)
-#                     y[client] = np.append(y[client], i * np.ones(num_sample), axis=0)
-#                 idx += num_sample
-#                 statistic[client].append((i, num_sample))
-#                 class_num_client[client] -= 1
-#     elif niid and partition == "dir":
-#         # https://github.com/IBM/probabilistic-federated-neural-matching/blob/master/experiment.py
-#         min_size = 0
-#         K = num_classes
-#         N = len(dataset_label)
-#         net_dataidx_map = {}
-#         while min_size < least_samples:
-#             idx_batch = [[] for _ in range(num_clients)]
-#             for k in range(K):
-#                 idx_k = np.where(dataset_label == k)[0]
-#                 np.random.shuffle(idx_k)
-#                 proportions = np.random.dirichlet(np.repeat(alpha, num_clients))
-#                 ## Balance
-#                 proportions = np.array([p * (len(idx_j) < N / num_clients) for p, idx_j in zip(proportions, idx_batch)])
-#                 proportions = proportions / proportions.sum()
-#                 proportions = (np.cumsum(proportions) * len(idx_k)).astype(int)[:-1]
-#                 idx_batch = [idx_j + idx.tolist() for idx_j, idx in zip(idx_batch, np.split(idx_k, proportions))]
-#                 min_size = min([len(idx_j) for idx_j in idx_batch])
-#         for j in range(num_clients):
-#             np.random.shuffle(idx_batch[j])
-#             net_dataidx_map[j] = idx_batch[j]
-#         # additional codes
-#         for client in range(num_clients):
-#             idxs = net_dataidx_map[client]
-#             X[client] = dataset_content[idxs]
-#             y[client] = dataset_label[idxs]
-#
-#             for i in np.unique(y[client]):
-#                 statistic[client].append((int(i), int(sum(y[client] == i))))
-#     else:
-#         raise EOFError
-#     del data
-#     for client in range(num_clients):
-#         print(f"Client {client}\t Size of data: {len(X[client])}\t Labels: ", np.unique(y[client]))
-#         print(f"\t\t Samples of labels: ", [i for i in statistic[client]])
-#         print("-" * 50)
-#     return X, y, statistic
-#
-#
-# def load_mnist_data():
-#     trans_train_mnist = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-#     trans_test_mnist = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-#     mnist_train_ds = torchvision.datasets.MNIST('../Data/mnist/', train=True, download=True,
-#                                                 transform=trans_train_mnist)
-#     mnist_test_ds = torchvision.datasets.MNIST('../Data/mnist/', train=False, download=True, transform=trans_test_mnist)
-#     X_train, y_train = mnist_train_ds.data, mnist_train_ds.targets
-#     X_test, y_test = mnist_test_ds.data, mnist_test_ds.targets
-#     X_train = X_train.data.numpy()
-#     y_train = y_train.data.numpy()
-#     X_test = X_test.data.numpy()
-#     y_test = y_test.data.numpy()
-#     return (X_train, y_train, X_test, y_test)
-#
-#
-# def load_cifar10_data():
-#     transform_train_cifar10 = transforms.Compose([
-#         transforms.RandomHorizontalFlip(),
-#         transforms.ToTensor(),
-#         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-#     ])
-#     transform_test_cifar10 = transforms.Compose([
-#         transforms.ToTensor(),
-#         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-#     ])
-#     cifar10_train_ds = torchvision.datasets.CIFAR10('../Data/cifar', train=True, download=True,
-#                                                     transform=transform_train_cifar10)
-#     cifar10_test_ds = torchvision.datasets.CIFAR10('../Data/cifar', train=False, download=True,
-#                                                    transform=transform_test_cifar10)
-#     X_train, y_train = cifar10_train_ds.data, cifar10_train_ds.targets
-#     X_test, y_test = cifar10_test_ds.data, cifar10_test_ds.targets
-#     return X_train, y_train, X_test, y_test
-#
-#
-# def record_net_data_stats(y_train, net_dataidx_map):
-#     net_cls_counts = {}
-#     for net_i, dataidx in net_dataidx_map.items():
-#         unq, unq_cnt = np.unique(y_train[dataidx], return_counts=True)
-#         tmp = {unq[i]: unq_cnt[i] for i in range(len(unq))}
-#         net_cls_counts[net_i] = tmp
-#     return net_cls_counts
-#
-# # https://github.com/IBM/probabilistic-federated-neural-matching/blob/master/datasets.py
-# def partition_data(dataset, partition, n_nets, alpha=0.5):
-#     if dataset == 'mnist':
-#         X_train, y_train, X_test, y_test = load_mnist_data()
-#     elif dataset == 'cifar10':
-#         X_train, y_train, X_test, y_test = load_cifar10_data()
-#     n_train = X_train.shape[0]
-#     if partition == "homo":
-#         idxs = np.random.permutation(n_train)
-#         batch_idxs = np.array_split(idxs, n_nets)
-#         net_dataidx_map = {i: batch_idxs[i] for i in range(n_nets)}
-#     elif partition == "hetero-dir":
-#         min_size = 0
-#         K = 10
-#         N = y_train.shape[0]
-#         net_dataidx_map = {}
-#         while min_size < 10:
-#             idx_batch = [[] for _ in range(n_nets)]
-#             print(idx_batch)
-#             for k in range(K):
-#                 idx_k = np.where(y_train == k)[0]
-#                 np.random.shuffle(idx_k)
-#                 proportions = np.random.dirichlet(np.repeat(alpha, n_nets))
-#                 proportions = np.array([p * (len(idx_j) < N / n_nets) for p, idx_j in zip(proportions, idx_batch)])
-#                 proportions = proportions / proportions.sum()
-#                 proportions = (np.cumsum(proportions) * len(idx_k)).astype(int)[:-1]
-#                 idx_batch = [idx_j + idx.tolist() for idx_j, idx in zip(idx_batch, np.split(idx_k, proportions))]
-#                 min_size = min([len(idx_j) for idx_j in idx_batch])
-#         for j in range(n_nets):
-#             np.random.shuffle(idx_batch[j])
-#             net_dataidx_map[j] = idx_batch[j]
-#     traindata_cls_counts = record_net_data_stats(y_train, net_dataidx_map)
-#     return X_train, y_train, X_test, y_test, net_dataidx_map, traindata_cls_counts
-#
-# if __name__ == '__main__':
-#     trans_train_mnist = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-#     dataset_train = torchvision.datasets.MNIST('../Data/mnist/', train=True, download=True, transform=trans_train_mnist)
-#     x, y, stat = separate_data((dataset_train.data, dataset_train.targets), 10, 10)
-#     import pprint
-#     X_train, y_train, X_test, y_test, net_dataidx_map, traindata_cls_counts = partition_data(dataset='mnist', partition='hetero-dir', n_nets=10, alpha=0.5)
-#     pprint.pprint(traindata_cls_counts)
+    transform_train_cifar10 = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    transform_test_cifar10 = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    cifar10_train_ds = torchvision.datasets.CIFAR10('../Data/cifar', train=True, download=True, transform=transform_train_cifar10)
+    cifar10_test_ds = torchvision.datasets.CIFAR10('../Data/cifar', train=False, download=True, transform=transform_test_cifar10)
+    cifar10_client_idx_dict, cifar10_client_cls_counts = get_cifar10_noniid(cifar10_train_ds, mode="dir", num_clients=10)
+    # cifar10_client_idx_dict, cifar10_client_cls_counts = get_cifar10_iid(cifar10_train_ds, mode="homo", num_clients=10)
+    import pprint
+    pprint.pprint(cifar10_client_cls_counts)
